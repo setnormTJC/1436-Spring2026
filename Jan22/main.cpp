7381becfa7e8c02f03f1f99841eb2cce802ddb7e
Work <Work@MSI>
2026-01-29 09:07:58 -0600

    Add project files.

---
+++ Jan22/main.cpp
@@ -0,0 +1,61 @@
+﻿#include<iostream> 
+#include <string> 
+
+using namespace std; // I wonder what this thing does???
+
+
+int main()
+{
+	//notice that this is indented (one level)
+		//this is TWO levels of indentation
+
+	
+	cout << "Enter your (user)name:\n";
+	string username; //this is called a "variable declaration" (declare that this variable exists)
+	//what this does is 
+	getline(cin, username);
+
+	cout << "You entered this username: " << username << "\n";
+
+	//the rest of this program is going to be a calculator 
+	cout << "Enter the arithmetic expression you want to calculate the result of (ex: 5 + 2):\n";
+	cout << "This program will determine the result (ex: 5 + 2 = 7):\n";
+
+	int theFirstNumber; 
+	int the_second_number; //examples of ints: -234, 0, 85959
+	char theOperation; //char is not charcoal - it's short for CHARACTER (not in an RPG)
+
+	//5 + 2
+	cin >> theFirstNumber >> theOperation >> the_second_number;
+
+	cout << "Echoing your input back at you (to verify that we read it in correctly)\n";
+	
+	// drawl a box
+	//cout << "┌ \n"; // test if the computer allows use of these symbols
+
+	cout << "The first number is: " << theFirstNumber <<
+		" The operation is: " << theOperation << " The second number is: " <<
+		the_second_number; // this is ok to press enter to seperate the info
+
+
+	if (theOperation == '+')
+	{
+		cout << "\n\nADDING those 2 numbers gives: " << theFirstNumber + the_second_number << "\n";
+	}
+
+	else if (theOperation == '-')
+	{
+		cout << "\n\nSUBTRACTING those 2 numbers (which were " << theFirstNumber
+			<< " and " << the_second_number << " gives the result: "
+			<< theFirstNumber - the_second_number << "\n";
+	}
+
+	if (theOperation == '*')
+	{
+		cout << "\n\nMULTIPLYING those 2 numbers (which were " << theFirstNumber
+			<< " and " << the_second_number << " gives the result: "
+			<< theFirstNumber * the_second_number << "\n";
+	}
+
+	return 0; // completed successfully
+}
---
+++ Jan22/studentBrilliance.h
@@ -0,0 +1,47 @@
+#pragma once
+
+
+//void doBrilliantStudentThings()
+//{
+//  string userName = ""    getline(cin, username)  // getline and store it inside the variable string 
+// the getline can store as much information as you want (of course there are limitations for significant info)
+//	std::string theString = "";// i want to make a string variable so that we can record what the user want to see
+//	const char SENTINAL = NULL;
+//	int timesForcedIntoLoop;
+//
+//	while (SENTINAL != Q)
+//	{
+//		cout << "Helooooo????";
+//		cout << "We are in the program\n";
+//		cout << "asfasdfasfasdfasfd\n";
+//
+//		cout << "What do you want to type?" << endl << endl;
+//		cin >> theString;
+//
+//		cout << theString << endl << endl; // lets put this inside a while loop so he can keep saying things
+//
+//		cout << "Do you want to continue? (q to quit)\n";
+//		cin >> SENTINAL;
+//
+//		if (timesForcedIntoLoop = 1)
+//		{
+//			SENTINAL = ''
+//				timesForcedIntoLoop = 1;
+//			cout << "You silly fool, I was born in the darkness!/n"
+//				cout << "You were nearly molded by it/n"
+//
+//				cout << "Now do you want to quit the program?(Q to quit)/n"
+//				cin >> SENTINAL
+//		}
+//	}
+//
+//	cout << "You decided to leave me! I can not allow this!!!!!!\n" // ;ets force the user to have to stay here forever
+//
+//		//PUT THEM BACK INTO THE WHILE LOOP
+//		if (timesForcedIntoLoop == 0)
+//		{
+//			goto 15; // performs an unconditional jump to a specified label within the same function
+//			timesForcedIntoLoop = 1; // sets a variable so that they dont go to the same outputs
+//		}
+//
+//}
