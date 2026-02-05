e8cd3bb47349ab0817c4c9419c01c820e4664dce
Work <Work@MSI>
2026-02-05 10:37:05 -0600

    Where we ended on Feb. 5

---
+++ SimpleFunctionDemo/SimpleFunctionDemo.cpp
@@ -0,0 +1,53 @@
+// SimpleFunctionDemo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include<Windows.h>
+
+using namespace std; 
+
+//functions have a ... function (they DO stuff)
+void doStuff()
+{
+    //notice! this text is INDENTED!
+    cout << "Doing .... stuff\n"; //\n moves the cursor to the next line (makes space)
+    //OMINOUS!
+}
+
+void showCatVideo()
+{
+    system("prayKitty.mp4");
+    //return asdfkjsadf,jfd,kdsaflk //not really needed
+}
+
+void playAHOTBeat() 
+{
+    std::system("PS1.wav");//this is a function DEFINITION!
+}
+
+void pauseFor5Seconds()
+{
+    Sleep(5'000); 
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    doStuff(); 
+
+    showCatVideo(); //this is a "function call" (that is a VERY IMPORTANT bit of jargon/vocabulary)
+    
+    pauseFor5Seconds(); 
+
+
+    playAHOTBeat(); 
+
+    //std::system("pause");
+
+    //std::system("calc");
+
+
+
+}
+
