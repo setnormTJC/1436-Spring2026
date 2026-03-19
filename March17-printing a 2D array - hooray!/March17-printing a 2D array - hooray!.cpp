f44ecdc996e463e61ccbf75095bf92a9c0c5475b
Work <Work@MSI>
2026-03-19 09:33:40 -0500

    Mid-class break on March 19.

--- March17-printing a 2D array - hooray!/March17-printing a 2D array - hooray!.cpp
+++ March17-printing a 2D array - hooray!/March17-printing a 2D array - hooray!.cpp
@@ -31,7 +31,45 @@ int main()
 		{"0", "0", "1"} //third row 
 	};
 
-	printTheGrid(theGrid);
+	//printTheGrid(theGrid);
+
+
+	bool isGameOver = true;// , false; ("simple data type") ("non-simple" -> array (a list of stuff) 
+
+	isGameOver = !isGameOver; //flip flop (toggle)
+
+	/*homogeneous data structures (lists) -> arrays are "complex" data types (because more than one value is stored)*/
+	vector<string> someEmotions =
+	{
+		"joy",
+		"disgust",
+		"sadness"
+	};
+
+
+	cout << "BEFORE inserting (AKA: pushing into the back) jealousy, the BACKMOST element is: "
+		<< someEmotions.back() << "\n";
+
+	someEmotions.push_back("jealousy!");
+
+	cout << "The number of emotions in our current list is: " << someEmotions.size() << "\n";
+
+	for (string currentEmotion : someEmotions)
+	{
+		//int a = 123; //this guy has "local scope" and CANNOT be accessed outside of this loop!
+		cout << currentEmotion << "\n";
+	}
+
+	//a = 123123123; //error!
+
+
+
+	//this is ANOTHER example of a complex (and also hetergeneous data type)
+	//struct Person
+	//{
+	//	int age; 
+	//	std::string name; 
+	//};
 
 
 }
